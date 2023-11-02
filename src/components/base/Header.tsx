c6f1e601061e5c3c5f92c0907515c9f50ce628e1
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2023-11-02 13:24:51 -0300

    changing header style

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -1,8 +1,22 @@
+import { Box, Text } from "@chakra-ui/react";
+
 const Header = () => {
     return (
-        <header>
-            Dio Banking
-        </header>
+        <Box
+            minHeight='8vh'
+            backgroundColor='#3454D1'
+            display="flex"
+            alignItems="center"
+            paddingLeft='15px'
+            paddingRight='15px'
+            color={"white"}
+            justifyContent="space-between"
+
+        >
+            <Text as="b" letterSpacing="1px" color="#FFFFFF" fontSize="1xl">
+                Dio Banking
+            </Text>
+        </Box>
     );
 }
 
